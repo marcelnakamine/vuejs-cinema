f0dc8eff34ce98d8c81ecd4e599f7c5f4e0d5881
Marcel Nakamine <marcel.nakamine@gmail.com>
2017-09-25 15:19:59 -0300

    Creating logic for genre filter

--- src/main.js
+++ src/main.js
@@ -23,15 +23,29 @@ new Vue({
   components: {
     'movie-list': {
       template: `<div id="movie-list">
-                  <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+                  <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
                 </div>`,
       data: function() {
         return {
           movies: [
-            { title: 'Pulp Fiction' },
-            { title: 'Home Alone' },
-            { title: 'Austin Powers' }
+            { title: 'Pulp Fiction', genre: genres.CRIME },
+            { title: 'Home Alone', genre: genres.COMEDY },
+            { title: 'Austin Powers', genre: genres.COMEDY }
           ]
+        }
+      },
+      props: ['genre', 'time'],
+      methods: {
+        moviePassesGenreFilter(movie) {
+          if(!this.genre.length) {
+            return true;
+          }
+          return this.genre.find(genre => movie.genre === genre);
+        }
+      },
+      computed: {
+        filteredMovies() {
+          return this.movies.filter(this.moviePassesGenreFilter);
         }
       }
     },
