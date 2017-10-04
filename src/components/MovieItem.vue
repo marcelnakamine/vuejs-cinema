a88c20ef561ce642bd32db56d6d21e646358e80e
Marcel Nakamine <marcel.nakamine@gmail.com>
2017-10-04 13:26:44 -0300

    adding rating

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div class="movie">
     <div class="movie-col-left">
       <img v-bind:src="movie.Poster">
     </div>
     <div class="movie-col-right">
       <div class="movie-title">
         <h2>{{ movie.Title }}</h2>
-        <span class="movie-rating">{{ movie.Rated}}</span>
+        <span class="movie-rating">{{ movie.Rated }}</span>
       </div>
     </div>
   </div>
